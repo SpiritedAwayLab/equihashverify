6e07d0e82e054dc04c187af881213b5f6e353c85
Fei <splxsgmobile@gmail.com>
2022-11-12 03:43:30 +0800

    123

--- equihashverify.cc
+++ equihashverify.cc
@@ -8,67 +8,74 @@
 #include <vector>
 using namespace v8;
 
-const char* ToCString(const String::Utf8Value& value) {
-  return *value ? *value : "<string conversion failed>";
+const char *ToCString(const String::Utf8Value &value)
+{
+    return *value ? *value : "<string conversion failed>";
 }
 
-int verifyEH(const char *hdr, const std::vector<unsigned char> &soln, const char *personalizationString, unsigned int N, unsigned int K) {
+int verifyEH(const char *hdr, const std::vector<unsigned char> &soln, const char *personalizationString, unsigned int N, unsigned int K)
+{
     // Hash state
     crypto_generichash_blake2b_state state;
     EhInitialiseState(N, K, state, personalizationString);
 
-    crypto_generichash_blake2b_update(&state, (const unsigned char*)hdr, 204);
+    crypto_generichash_blake2b_update(&state, (const unsigned char *)hdr, 204);
 
     bool isValid;
-    
+
     EhIsValidSolution(N, K, state, soln, isValid);
 
-	return isValid;
+    return isValid;
 }
 
-void Verify(const v8::FunctionCallbackInfo<Value>& args) {
-    Isolate* isolate = Isolate::GetCurrent();
+void Verify(const v8::FunctionCallbackInfo<Value> &args)
+{
+    Isolate *isolate = Isolate::GetCurrent();
     HandleScope scope(isolate);
-    printf("lalalalal####################################");
-    if (args.Length() < 4) {
+    printf("lalalalal####################################\n");
+    if (args.Length() < 4)
+    {
         isolate->ThrowException(
-            Exception::TypeError(String::NewFromUtf8(isolate, "Wrong number of arguments"))
-        );
+            Exception::TypeError(String::NewFromUtf8(isolate, "Wrong number of arguments")));
 
         return;
     }
-
-    if (!args[3]->IsInt32() || !args[4]->IsInt32()) {
+    printf("lalalalal####################################1\n");
+    if (!args[3]->IsInt32() || !args[4]->IsInt32())
+    {
         isolate->ThrowException(
-            Exception::TypeError(String::NewFromUtf8(isolate, "Fourth and fifth parameters should be equihash parameters (n, k)"))
-        );
+            Exception::TypeError(String::NewFromUtf8(isolate, "Fourth and fifth parameters should be equihash parameters (n, k)")));
 
         return;
     }
-
+    printf("lalalalal####################################2\n");
     Local<Object> header = args[0]->ToObject();
     Local<Object> solution = args[1]->ToObject();
 
-    if(!node::Buffer::HasInstance(header) || !node::Buffer::HasInstance(solution)) {
+    if (!node::Buffer::HasInstance(header) || !node::Buffer::HasInstance(solution))
+    {
         isolate->ThrowException(
-            Exception::TypeError(String::NewFromUtf8(isolate, "First two arguments should be buffer objects."))
-        );
+            Exception::TypeError(String::NewFromUtf8(isolate, "First two arguments should be buffer objects.")));
 
         return;
     }
-
-    if (!args[2]->IsString()) {
+    printf("lalalalal\n");
+    if (!args[2]->IsString())
+    {
         isolate->ThrowException(
-            Exception::TypeError(String::NewFromUtf8(isolate, "Third argument should be the personalization string."))
-        );
+            Exception::TypeError(String::NewFromUtf8(isolate, "Third argument should be the personalization string.")));
 
         return;
     }
-
+    printf("lalalalal\n");
     const char *hdr = node::Buffer::Data(header);
-    if(node::Buffer::Length(header) != 204) {
-        //invalid hdr length
-        args.GetReturnValue().Set(false);
+    printf("lalalalal\n");
+    if (node::Buffer::Length(header) != 204)
+    {
+        // invalid hdr length
+        printf("222222222222222204")
+            args.GetReturnValue()
+                .Set(false);
         return;
     }
 
@@ -77,7 +84,7 @@ void Verify(const v8::FunctionCallbackInfo<Value>& args) {
     std::vector<unsigned char> vecSolution(soln, soln + node::Buffer::Length(solution));
 
     String::Utf8Value str(args[2]);
-    const char* personalizationString = ToCString(str);
+    const char *personalizationString = ToCString(str);
 
     // Validate for N, K (4th and 5th parameters)
     bool result = verifyEH(
@@ -85,14 +92,13 @@ void Verify(const v8::FunctionCallbackInfo<Value>& args) {
         vecSolution,
         personalizationString,
         args[3].As<Uint32>()->Value(),
-        args[4].As<Uint32>()->Value()
-    );
+        args[4].As<Uint32>()->Value());
 
     args.GetReturnValue().Set(result);
 }
 
-
-void Init(Handle<Object> exports) {
+void Init(Handle<Object> exports)
+{
     NODE_SET_METHOD(exports, "verify", Verify);
 }
 
